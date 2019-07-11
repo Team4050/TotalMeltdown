1122e7eb04f262e7c6063c8eb166d19bba2181f6
Daniel <trainerlord@gmail.com>
2019-07-11 10:50:43 -0400

    Added Drive capabilities To Linear Acutator

--- LinearActuator/src/main/java/frc/robot/Robot.java
+++ LinearActuator/src/main/java/frc/robot/Robot.java
@@ -15,28 +15,56 @@ import edu.wpi.first.wpilibj.Joystick;
 
 
 public class Robot extends TimedRobot {
-
-  private Joystick xbox;
-
-  Spark LinearActuator = new Spark(3);
-  Spark ShootMotor = new Spark(4);
-
-  
-
-  @Override
-  public void robotInit() 
-  {
-    xbox = new Joystick(0);
-    LinearActuator = new Spark(3);
-    ShootMotor = new Spark(4);
-    
-    
+	//Drive Motor Variable init
+	private Spark FrontLeft;
+	private Spark FrontRight;
+	private Spark RearLeft;
+	private Spark RearRight;
+	//Joystick/Xbox Variable init
+	private Joystick xbox;
+	private Joystick leftStick;
+	private Joustick rightStick;
+	//LinearActuator Varible init
+	Spark LinearActuator = new Spark(3);
+	Spark ShootMotor = new Spark(4);
+	//Side Groups
+	private SpeedControllerGroup left;
+	private SpeedControllerGroup right;
+	//RobotDrive
+	private DifferentialDrive robotDrive;
+	@Override
+	public void robotInit() 
+	{
+		//assigning Data Values
+			//Joysticks
+		leftStick = new Joystick(0);
+		rightStick = new Joystick(1);
+		xbox = new Joystick(2);
+			//Drive Motors
+		FrontLeft = new Spark(0);
+		RearLeft = new Spark(1);
+		FrontRight = new Spark(2);
+		RearRight = new Spark(3);
+			//Actuator Motors
+		LinearActuator = new Spark(4);
+		ShootMotor = new Spark(5);
+		
+		//Invert Motors
+		FrontLeft.set(-1);
+		RearLeft.set(-1);
+		//Asign Speed Groups
+		right = new SpeedControllerGroup(FrontLeft,RearLeft);
+		left = new SpeedControllerGroup(FrontRight,RearRight);
+		//Asign The Drive system
+		robotDrive = new DifferentialDrive(left,right);
+		
   }
 
   
   @Override
   public void teleopPeriodic() 
   {
+		//Linear Actuator
     boolean buttonValue;
     int direction = xbox.getPOV(0);
     buttonValue = xbox.getRawButton(5);
@@ -58,7 +86,9 @@ public class Robot extends TimedRobot {
     } else 
     {
       LinearActuator.set(0);
-    }
+		}
+		//Drive Code
+		robotDrive.tankDrive(leftStick.getY(), rightStick.getY());
     
   }
 
